c4d05566068ed4479103a335d5a8557f3af37531
Charles Vargas <jcc.devweb@gmail.com>
2022-12-12 12:41:52 +0800

    implement className Navbar.js ready to style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import ErrorPage from "./Pages/ErrorPage"
 import LoginForm from "./components/LoginForm"
-
+import './index.css'
 const App = () => {
   return (
     <>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="main-navbar">
-      <ul>
-        <li><Link to="/"> Home </Link>
+      <h1 className="navbar-brand"> Brand Here</h1>
+      <ul className="list">
+        <li className="list-item">
+          <Link className="links" to="/">
+            Home
+          </Link>
         </li>
-        <li><Link to="/About"> About  </Link>
+        <li className="list-item">
+          <a className="links">
+            <Link className="links" to="/About">
+              About
+            </Link>{" "}
+          </a>
         </li>
-        <li><Link to="/Contact"> Contact  </Link>
+        <li className="list-item">
+          <Link className="links" to="/Contact">
+            Contact
+          </Link>
         </li>
-        <li><Link to="/LoginForm"> Register  </Link>
+        <li className="list-item">
+          <Link className="links" to="/LoginForm">
+            Register
+          </Link>
         </li>
-       
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
